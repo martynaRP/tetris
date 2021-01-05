efaa7d148286359340cd2cae3114b50765e9a9b8
lukaszspyra <lukasz.spyrka@gmail.com>
2021-01-05 12:01:02 +0100

    Create L-shaped block with tests
    Created block consist of 0 and 1 representing dots and empty spaces,
    tests run from testng.xml  proves correct size and location of markings.

---
+++ src/main/java/com/epam/prejap/tetris/block/LBlock.java
@@ -0,0 +1,22 @@
+package com.epam.prejap.tetris.block;
+
+/**
+ * The LBlock class represents block shape of "L" in tetris game.
+ *
+ * <p>Implementation note: It contains byte 2d array with '1' and '0',
+ * arrange in a way to form required shape.
+ *
+ */
+final class LBlock extends Block{
+
+    private static final byte[][] IMAGE = {
+            {1, 0},
+            {1, 0},
+            {1, 1},
+    };
+
+    LBlock() {
+        super(IMAGE);
+    }
+
+}
