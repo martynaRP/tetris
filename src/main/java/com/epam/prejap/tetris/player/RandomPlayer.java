149bbc935f7cd6a8bdf9f8d14906eb1aa789b746
Magdalena Zaba <magdalenazaba@tlen.pl>
2021-01-06 22:32:46 +0100

    Reformat files

--- src/main/java/com/epam/prejap/tetris/player/RandomPlayer.java
+++ src/main/java/com/epam/prejap/tetris/player/RandomPlayer.java
@@ -1,23 +1,25 @@
 package com.epam.prejap.tetris.player;
 
 import com.epam.prejap.tetris.game.Move;
+
 import java.util.Optional;
 import java.util.Random;
 
 public class RandomPlayer implements Player {
 
-    private final Random random ;
+    private final Random random;
 
-    public RandomPlayer(Random random){
-        this.random=random;
+    public RandomPlayer(Random random) {
+        this.random = random;
     }
 
-    /** Generates random move for Player from all available defined in Move enum
+    /**
+     * Generates random move for Player from all available defined in Move enum
+     *
      * @return Optional with Move value
      */
     @Override
     public Optional<Move> nextMove() {
         return Optional.of(Move.values()[random.nextInt(Move.values().length)]);
     }
-
 }
--- src/test/java/com/epam/prejap/tetris/player/RandomPlayerTest.java
+++ src/test/java/com/epam/prejap/tetris/player/RandomPlayerTest.java
@@ -3,16 +3,19 @@ package com.epam.prejap.tetris.player;
 import com.epam.prejap.tetris.game.Move;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
 import java.util.Random;
+
 import static org.testng.Assert.*;
 
 @Test(groups = "Player")
 public class RandomPlayerTest {
 
-    class MockRandom extends Random{
+    class MockRandom extends Random {
         private final int mockRandomInt;
-        MockRandom(int mockRandomInt){
-            this.mockRandomInt=mockRandomInt;
+
+        MockRandom(int mockRandomInt) {
+            this.mockRandomInt = mockRandomInt;
         }
 
         @Override
@@ -22,18 +25,17 @@ public class RandomPlayerTest {
     }
 
     @DataProvider
-    public Object[] allMoveEnumValues(){
+    public Object[] allMoveEnumValues() {
         return Move.values();
     }
 
-
-    @Test( dataProvider = "allMoveEnumValues")
+    @Test(dataProvider = "allMoveEnumValues")
     public void nextMoveShouldGenerateEachPossibleValue(Move expectedMove) {
         Random mockRandom = new MockRandom(expectedMove.ordinal());
-        RandomPlayer testingRandomizer=new RandomPlayer(mockRandom);;
+        RandomPlayer testingRandomizer = new RandomPlayer(mockRandom);
 
         var actualMove = testingRandomizer.nextMove().get();
 
-        assertEquals(actualMove,expectedMove);
+        assertEquals(actualMove, expectedMove);
     }
 }
