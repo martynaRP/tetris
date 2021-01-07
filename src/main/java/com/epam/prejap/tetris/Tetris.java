864c6c2b48c9e850ecbadda913d5a40aae6ae700
martynaRP <rucinska.martyna@gmail.com>
2021-01-07 18:37:39 +0100

    investigate command-line input for custom key configuration validate input in transformArgToKeys method if it's not null neither blank throw IllegalArgumentException with proper message if invalid input provided

--- src/main/java/com/epam/prejap/tetris/Tetris.java
+++ src/main/java/com/epam/prejap/tetris/Tetris.java
@@ -53,9 +53,35 @@ class Tetris {
         var playfield = new Playfield(rows, cols, feed, printer);
         var game = new Tetris(playfield, new Waiter(delay), new RandomPlayer(new Random()));
 
+        if (args != null && args.length != 0) {
+            checkArgsForNavigationKeys(args[0]);
+        }
+
         var score = game.play();
 
         System.out.println("Score: " + score.points());
     }
 
+    public static char[] checkArgsForNavigationKeys(String arg0) {
+        char[] keys = Move.defaultKeys();
+        if (arg0 != null && !arg0.isBlank()) {
+            char[] customKeys = transformArgToKeys(arg0);
+            keys = Move.modifyNavigationKeys(customKeys);
+            System.out.println("Custom navigation keys configured with success: \n" +
+                    Move.navigationKeysConfiguration());
+        }
+        return keys;
+    }
+
+    private static char[] transformArgToKeys(String arg0) {
+        String[] args = arg0.replaceAll("\\s+", " ").trim().split(" ");
+        final int numberOfNavigationKeys = Move.values().length;
+        if (args.length == numberOfNavigationKeys) {
+            char[] keys = arg0.replaceAll("\\s", "").toCharArray();
+            if (keys.length == numberOfNavigationKeys) {
+                return keys;
+            } else throw new IllegalArgumentException("Each navigation key should be represented by a single " +
+                    "character.");
+        } else throw new IllegalArgumentException("Incorrect amount of values provided for navigation keys.");
+    }
 }
