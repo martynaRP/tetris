e32a827ab8dd48db808ae4fb9caec1588e5ad306
martynaRP <rucinska.martyna@gmail.com>
2021-01-07 18:16:08 +0100

    make navigation keys in Move configurable - remove final keyword from the variable key - create modifyNavigationKeys method that assigns a new key to each Enum value based on a provided keys - helper method GetNavigationKeys that returns a set of current navigation keys - navigationKeysConfiguration method creates a string representation of moves and associated keys - initial value of the key for each move is declared in defaultKeys() method

--- src/main/java/com/epam/prejap/tetris/game/Move.java
+++ src/main/java/com/epam/prejap/tetris/game/Move.java
@@ -1,13 +1,15 @@
 package com.epam.prejap.tetris.game;
 
+import java.util.Arrays;
+
 public enum Move {
 
-    NONE(' '),
-    LEFT('h'),
-    RIGHT('l'),
+    NONE(defaultKeys()[0]),
+    LEFT(defaultKeys()[1]),
+    RIGHT(defaultKeys()[2]),
     ;
 
-    private final int key;
+    private int key;
 
     Move(int key) {
         this.key = key;
@@ -20,5 +22,25 @@ public enum Move {
             }
         }
         return NONE;
+    }
+
+    public static char[] defaultKeys() {
+        return new char[]{' ', 'h', 'l'};
+    }
+
+    public static char[] modifyNavigationKeys(char[] keys) {
+        Arrays.stream(values()).forEach(val -> val.key = keys[val.ordinal()]);
+        return Move.getNavigationKeys();
+    }
+
+    private static char[] getNavigationKeys() {
+        char[] navigationKeys = new char[values().length];
+        Arrays.stream(values()).forEach(val -> navigationKeys[val.ordinal()] = (char) val.key);
+        return navigationKeys;
+    }
+
+    public static String navigationKeysConfiguration() {
+        return Arrays.deepToString(Arrays.stream(values())
+                .map(val -> "" + val + ": " + (char) val.key).toArray());
     }
 }
